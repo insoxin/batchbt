ee3c5a859022c35e7bd0cc9a96c01f5073156fe8
Redy <19371836+insoxin@users.noreply.github.com>
2021-09-24 13:21:09 +0800

    Update batchbt.sh

--- batchbt.sh
+++ batchbt.sh
@@ -9,14 +9,4 @@ fi
 
 . $public_file
 download_Url=$NODE_URL
-# 此脚本会自动在服务器上安装防火墙、任务管理器、基线扫描等插件（改插件名称就可以）
-plugins=("btwaf" "task_manager" "san_security" "bt_security" "resource_manager" "syssafe")
-for ((i=0;i<$#plugins[@];i++))
-do
-    cd /tmp
-    plugin_download_url="${download_Url}/install/plugin/${plugins[i]}/install.sh"
-    echo $plugin_download_url
-    wget -O "${plugins[i]}_install.sh" $plugin_download_url
-    sh "${plugins[i]}_install.sh" install
-    rm -rf "${plugins[i]}_install.sh"
 done
